42c5001e0b92d6538e8e7c74155567f2cfeb430e
Alexander Skoglund <alexander.skoglund@gmail.com>
2012-01-23 14:11:04 +0100

    Added tools.c/h

---
+++ avt/tools.h
@@ -0,0 +1,34 @@
+
+#ifndef __TOOLS_H__
+#define __TOOLS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+//#include <X11/Xlib.h>
+//#include <X11/Xutil.h>
+//#include <X11/extensions/Xvlib.h>
+//#include <X11/keysym.h>
+#define _GNU_SOURCE_
+#include <getopt.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+/* SDL and OpenGL */
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <SDL.h>
+
+
+
+
+
+#endif
